374b688c3f6c46fb1bc4addce33aa025ac7f128e
Hayden Sartoris <hsartoris@bard.edu>
2019-08-28 10:43:03 -0400

    hopefully effective parsing

--- lib/ueberauth/strategy/cas/user.ex
+++ lib/ueberauth/strategy/cas/user.ex
@@ -3,28 +3,58 @@ defmodule Ueberauth.Strategy.CAS.User do
   Representation of a CAS user with their roles.
   """
 
-  defstruct name: nil, email: nil, roles: nil
+  defstruct principal: nil, 
+    employeeType: nil, 
+    email: nil, 
+    first_name: nil, 
+    last_name: nil, 
+    name: nil,
+    eduPersonPrincipalName: nil,
+    nickname: nil
+
+  defp attr_map do
+    %{
+      :principal => "cas:user",
+      :employeeType => "/cas:employeeType",
+      :email => "/cas:mail",
+      :first_name => "/cas:givenName",
+      :last_name => "/cas:sn",
+      :name => "/cas:cn",
+      :eduPersonPrincipalName => "/cas:eduPersonPrincipalName",
+      :nickname => "/cas:uid"
+    }
+  end
 
   alias Ueberauth.Strategy.CAS.User
 
   def from_xml(body) do
     body |> IO.inspect
-    %User{}
-    |> set_name(body)
-    |> set_email(body)
-    |> set_roles(body)
+    %User{} |> assign_attrs(body, Map.keys(attr_map))
+    #%User{}
+    #|> set_name(body)
+    #|> set_email(body)
+    #|> set_roles(body)
   end
 
-  defp set_name(user, body),   do: %User{user | name: email(body)}
-  defp set_email(user, body),  do: %User{user | email: email(body)}
-  defp set_roles(user, _body), do: %User{user | roles: ["developer", "admin"]}
-
-  defp email(body) do
-    Floki.find(body, "cas|user")
-    |> List.first
-    |> Tuple.to_list
-    |> List.last
-    |> List.first
-    |> String.downcase
+  defp assign_attrs(user, _body, []), do: user
+  defp assign_attrs(user, body, [key | tail]) do
+    %User{user | key => get_attr(body, key)} |> assign_attrs(body, tail)
   end
+
+  defp get_attr(body, attr), do: body |> xpath(~x"//#{attr}/text()")
+
+
+    #defp set_name(user, body),   do: %User{user | name: email(body)}
+    #defp set_email(user, body),  do: %User{user | email: email(body)}
+    #defp set_roles(user, _body), do: %User{user | roles: ["developer", "admin"]}
+
+    #defp email(body) do
+    #  Floki.find(body, "cas|user")
+    #  |> List.first
+    #  |> Tuple.to_list
+    #  |> List.last
+    #  |> List.first
+    #  |> String.downcase
+    #end
+
 end
