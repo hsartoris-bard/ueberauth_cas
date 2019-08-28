9623a890d7d8111aef2c1919efc2cf6c2fc840cd
Hayden Sartoris <hsartoris@bard.edu>
2019-08-28 13:46:15 -0400

    now with cruft removed and a spare attribute added to test resilience

--- lib/ueberauth/strategy/cas/user.ex
+++ lib/ueberauth/strategy/cas/user.ex
@@ -20,21 +20,9 @@ defmodule Ueberauth.Strategy.CAS.User do
     givenName: nil,
     sn: nil,
     cn: nil,
-    eduPersonPrincipalName: nil
+    eduPersonPrincipalName: nil,
+    udc_identifier: nil
   ]
-
-  #defp attr_map do
-  #  %{
-  #    :principal => "cas:user",
-  #    :employeeType => "/cas:employeeType",
-  #    :mail => "/cas:mail",
-  #    :first_name => "/cas:givenName",
-  #    :last_name => "/cas:sn",
-  #    :name => "/cas:cn",
-  #    :eduPersonPrincipalName => "/cas:eduPersonPrincipalName",
-  #    :nickname => "/cas:uid"
-  #  }
-  #end
 
   alias Ueberauth.Strategy.CAS.User
 
@@ -52,48 +40,5 @@ defmodule Ueberauth.Strategy.CAS.User do
     IO.inspect(opts)
     
     %User{} |> struct(opts)
-    #|> set_attrs(body)
-    #|> struct(assign_attrs(%{}, body, Map.keys(attr_map())))
-    #|> struct(opts)
-
-    #%User{}
-    #|> set_name(body)
-    #|> set_email(body)
-    #|> set_roles(body)
   end
-
-  #defp set_attrs(user, body) do
-  #  %User{user | user: get_attr(body, "/cas:uid")}
-  #  %User{user | employeeType: get_attr(body, "/cas:employeeType")}
-  #  %User{user | mail: get_attr(body, "/cas:mail")}
-  #  %User{user | givenName: get_attr(body, "/cas:givenName")}
-  #  %User{user | sn: get_attr(body, "/cas:sn")}
-  #  %User{user | cn: get_attr(body, "/cas:cn")}
-  #  %User{user | eduPersonPrincipalName: get_attr(body, "/cas:eduPersonPrincipalName")}
-  #  user
-  #end
-
-  #defp assign_attrs(attrs, _body, []), do: attrs
-  #defp assign_attrs(attrs, body, [key | tail]) do
-  #  %{^key => value} = body
-  #  %{attrs | key => get_attr(body, value)} 
-  #  |> assign_attrs(body, tail)
-  #end
-
-  #defp get_attr(body, attr), do: body |> xpath(~x"//#{attr}/text()")
-
-
-    #defp set_name(user, body),   do: %User{user | name: email(body)}
-    #defp set_email(user, body),  do: %User{user | email: email(body)}
-    #defp set_roles(user, _body), do: %User{user | roles: ["developer", "admin"]}
-
-    #defp email(body) do
-    #  Floki.find(body, "cas|user")
-    #  |> List.first
-    #  |> Tuple.to_list
-    #  |> List.last
-    #  |> List.first
-    #  |> String.downcase
-    #end
-
 end
